a2dceae66e8e88ff4a1391e91bf034f2b71724e8
pH2O4 <ph2oalencar@gmail.com>
2022-04-04 23:37:18 -0300

    ----

--- node back/app.js
+++ node back/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const createError = require('http-errors');
 const morgan = require('morgan');
 require('dotenv').config();
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
--- node back/routes/api.route.js
+++ node back/routes/api.route.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
